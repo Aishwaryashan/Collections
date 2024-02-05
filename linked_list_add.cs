c8b4e1b43c2517f6ff357cb1683a6b4aef3327ee
Aishwaryashan <112060518+Aishwaryashan@users.noreply.github.com>
2024-02-05 07:35:22 +0530

    Update linked_list_add.cs

--- linked_list_add.cs
+++ linked_list_add.cs
@@ -5,15 +5,14 @@ public class LinkedListExample
 {  
     public static void Main(string[] args)  
     {  
-        // Create a list of strings  
+       
         var names = new LinkedList<string>();  
         names.AddLast("Sonoo Jaiswal");  
         names.AddLast("Ankit");  
         names.AddLast("Peter");  
         names.AddLast("Irfan");  
-        names.AddFirst("John");//added to first index  
-  
-        // Iterate list element using foreach loop  
+        names.AddFirst("John");
+   
         foreach (var name in names)  
         {  
             Console.WriteLine(name);  
