8a595a11b35c8e91a3d314dad7751649ee63c4ab
Aishwaryashan <112060518+Aishwaryashan@users.noreply.github.com>
2024-02-05 08:56:47 +0530

    Update sorted_set_min_max.cs

--- sorted_set_min_max.cs
+++ sorted_set_min_max.cs
@@ -1,1 +1,31 @@
 
+
+
+using System;  
+using System.Collections.Generic;  
+  
+public class SortedSetExample  
+{  
+    public static void Main(string[] args)  
+    {  
+     
+        var names = new SortedSet<string>();  
+        names.Add("Thrish");  
+        names.Add("Tara");  
+        names.Add("Aara");  
+        names.Add("Lallu");  
+        names.Add("Balu");
+          
+        foreach (var name in names)  
+        {  
+            Console.WriteLine(name);  
+        }  
+       var maxElement = names.Max;
+        Console.WriteLine("\nMax element is : ");
+        Console.WriteLine(maxElement);
+        var minElement = names.Min;
+        Console.WriteLine("\nMin element is");
+          Console.WriteLine(minElement);
+        
+    }  
+}  
