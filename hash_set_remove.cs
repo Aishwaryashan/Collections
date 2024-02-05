2dd7b733be32e60772f07cdd612a2d6ff4f6fd5f
Aishwaryashan <112060518+Aishwaryashan@users.noreply.github.com>
2024-02-05 08:20:14 +0530

    Update hash_set_remove.cs

--- hash_set_remove.cs
+++ hash_set_remove.cs
@@ -1,1 +1,28 @@
 
+using System;  
+using System.Collections.Generic;  
+  
+public class HashSetExample  
+{  
+    public static void Main(string[] args)  
+    {  
+
+        var names = new HashSet<string>();  
+        names.Add("Aish");  
+        names.Add("Thrish");  
+        names.Add("Chandu");  
+        names.Add("Dwarak");  
+        names.Add("Nishi");  
+      
+        foreach (var name in names)  
+        {  
+            Console.WriteLine(name);  
+        }  
+      names.Remove("Chandu");
+      Console.WriteLine("Hash_set after removing element:");
+         foreach (var name in names)  
+        {  
+            Console.WriteLine(name);  
+        } 
+    }  
+} 
