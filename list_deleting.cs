9765ab4ea4b647df048901111ae366aee6291983
Aishwaryashan <112060518+Aishwaryashan@users.noreply.github.com>
2024-02-05 08:28:44 +0530

    Create list_deleting.cs

---
+++ list_deleting.cs
@@ -0,0 +1,27 @@
+using System;  
+using System.Collections.Generic;  
+  
+public class ListExample  
+{  
+    public static void Main(string[] args)  
+    {  
+        
+        var names = new List<string>();  
+        names.Add("aishwarya");  
+        names.Add("Thrishalini");  
+        names.Add("Nishitha");  
+        names.Add("Rachana");  
+  
+        foreach (var name in names)  
+        {  
+            Console.WriteLine(name);  
+        } 
+        names.Remove("Nishitha");
+        Console.WriteLine("\nAfter Removing nishita:");
+         foreach (var name in names)  
+        {  
+            Console.WriteLine(name);  
+        } 
+        
+    }  
+} 
