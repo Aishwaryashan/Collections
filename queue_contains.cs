2d99a54a74faa60be47817e2c70f388ce36367bd
Aishwaryashan <112060518+Aishwaryashan@users.noreply.github.com>
2024-02-05 08:41:29 +0530

    Update queue_contains.cs

--- queue_contains.cs
+++ queue_contains.cs
@@ -1,1 +1,30 @@
-
+using System;  
+using System.Collections.Generic;  
+  
+public class QueueExample  
+{  
+    public static void Main(string[] args)  
+    {  
+        Queue<string> names = new Queue<string>();  
+        names.Enqueue("Sonoo");  
+        names.Enqueue("Peter");  
+        names.Enqueue("James");  
+        names.Enqueue("Ratan");  
+        names.Enqueue("Irfan");  
+  
+        foreach (string name in names)  
+        {  
+            Console.WriteLine(name);  
+        }  
+  
+     bool containsElement = names.Contains("Irfan");
+     if(containsElement){
+         Console.WriteLine("names contains Irfan");
+     }
+     else{
+         Console.WriteLine("names does not contain Irfan");
+     }
+     
+        
+    }  
+}  
