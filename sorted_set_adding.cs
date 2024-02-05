25ed473c23d6496802f6a2d54eeb67ea253ed5da
Aishwaryashan <112060518+Aishwaryashan@users.noreply.github.com>
2024-02-05 07:23:58 +0530

    Create sorted_set_adding.cs

---
+++ sorted_set_adding.cs
@@ -0,0 +1,22 @@
+using System;  
+using System.Collections.Generic;  
+  
+public class SortedSetExample  
+{  
+    public static void Main(string[] args)  
+    {  
+        // Create a set of strings  
+        var names = new SortedSet<string>();  
+        names.Add("Sonoo");  
+        names.Add("Ankit");  
+        names.Add("Peter");  
+        names.Add("Irfan");  
+        names.Add("Ankit");//will not be added  
+          
+        // Iterate SortedSet elements using foreach loop  
+        foreach (var name in names)  
+        {  
+            Console.WriteLine(name);  
+        }  
+    }  
+}  
