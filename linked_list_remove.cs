ab78a91844e5b1ee8109127718053e826aef9531
Aishwaryashan <112060518+Aishwaryashan@users.noreply.github.com>
2024-02-05 08:09:58 +0530

    Create linked_list_remove.cs

---
+++ linked_list_remove.cs
@@ -0,0 +1,28 @@
+using System;  
+using System.Collections.Generic;  
+  
+public class LinkedListExample  
+{  
+    public static void Main(string[] args)  
+    {  
+       
+        var names = new LinkedList<string>();  
+        names.AddLast("Sonoo Jaiswal");  
+        names.AddLast("Ankit");  
+        names.AddLast("Peter");  
+        names.AddLast("Irfan");  
+        names.AddFirst("John");
+   
+        foreach (var name in names)  
+        {  
+            Console.WriteLine(name);  
+        }  
+      names.Remove("Ankit");
+      Console.WriteLine("\nList After removing Ankit :");
+         foreach (var name in names)  
+        {  
+            Console.WriteLine(name);  
+        }  
+
+    }  
+} 
